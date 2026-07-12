a2336425ed1892b40ee75e2549f34930718c9639
Gabor Goczan <goczangabor24@gmail.com>
2026-07-12 20:29:42 +0200

    Tests pass, html tree built

--- src/test_extract_markdown_images.py
+++ src/test_extract_markdown_images.py
@@ -101,6 +101,24 @@ class TestTextNodesConverter(unittest.TestCase):
             ], matches
         )
 
+class TestMarkdownToHTMLNode(unittest.TestCase):
+    def test_markdown_to_html_node(self):
+        node = blocks.markdown_to_html_node(
+            """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+"""
+        )
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+        
+
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
         matches = blocks.markdown_to_blocks(
